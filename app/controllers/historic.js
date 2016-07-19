f3cc9c6fa0c3e6bc6a82582208261b09b7460aa0
Aldenor Junior <aldenor.junior@tce.ro.gov.br>
2016-07-19 08:24:04 -0400

    Crud codes

--- app/controllers/historic.js
+++ app/controllers/historic.js
@@ -4,25 +4,37 @@ module.exports = function(app) {
 	var controller = {};
 
   controller.create = function(req, res) {
+  	console.log("req.body.userid");
+  	console.log(req.body.userid);
     var puserid = req.body.userid,
-				pdate = req.body.date;
+		pdate = req.body.date;
 
-  Historic.sync().then(function() {
-    var data = {
-      userid:  puserid,
-      date: pdate
-    }
+		Historic.sync().then(function() {
+			var data = { userid:  puserid, date: pdate }
+			Historic.create(data)
+			.then(function(newHistoric) {
+				res.json(newHistoric);
+      		});
+    	});
 
-    Historic.create(data).then(function(newHistoric) {
-      console.log(newHistoric.get({
-        plain: true
-      }));
-    });
-  });
+    res.sendStatus(200);
+    res.end();
 
-  res.sendStatus(200);
-  res.end();
+  };
 
-  }
+	controller.read = function(req, res) {
+
+		Historic.findAll({
+		    	order: [['date','DESC']]			
+		}).then(function(historics) {
+			res.json(historics);
+		});
+	};
+
+	controller.delete = function(req, res) {
+
+	};
+
+	return controller;
 
 };
